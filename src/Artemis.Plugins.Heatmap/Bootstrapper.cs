265e230732c718dcc1af8df46c475e9ef2756214
Casper Abosch <psyca4@exmail.nottingham.ac.uk>
2026-03-05 15:46:53 +0000

    added initial Artemis.Plugins.Heatmap from empty template

---
+++ src/Artemis.Plugins.Heatmap/Bootstrapper.cs
@@ -0,0 +1,22 @@
+using Artemis.Core;
+using Artemis.UI.Shared;
+
+namespace Artemis.Plugins.Heatmap;
+
+public class Bootstrapper : PluginBootstrapper
+{
+    public override void OnPluginLoaded(Plugin plugin)
+    {
+        
+    }
+
+    public override void OnPluginEnabled(Plugin plugin)
+    {
+        
+    }
+    
+    public override void OnPluginDisabled(Plugin plugin)
+    {
+        
+    }
+}
